604cbf4bb78c1be38f80ee3ad4fefd5ab1cc7646
Amanuel Moha <84667760+amannnnnyyyy@users.noreply.github.com>
2024-02-08 15:39:06 +0300

    Iteration update

---
+++ src/app/components/details/details.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent implements OnInit {
+ ngOnInit():void{
+ }
+}
---
+++ src/app/pipes/profit.pipe.ts
@@ -0,0 +1,8 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({name:'profit'})
+export class profitPipe implements PipeTransform{
+    transform(value: number, profitPercentage =10):string {
+        return (value*profitPercentage*1/100).toFixed(2)
+    }
+}
